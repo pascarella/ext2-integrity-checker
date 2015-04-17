d54bfc1c0e784f3b5b098c242d3554afa7e30db2
Dominic Pascarella <dominicpascarella@gmail.com>
2015-04-17 10:25:54 -0400

    Correct copies that don't match
    Correct copies of the superblock and block group descriptor table to
    match the master copy in block group 0

--- ext2checker.cc
+++ ext2checker.cc
@@ -1,8 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include <iostream>
 #include <cassert>
+#include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 #include "ext2checker.h"
@@ -33,13 +33,13 @@ void printBGDT(BlockGroup *bg, u32 totalBlockGroups) {
          "-----------------------------------------------------------------\n");
   for (int i = 0; i < totalBlockGroups; i++) {
     printf("%5d %9u %9u %9u %9u %9u %9u\n",
-    i,
-    bg[i].bg_block_bitmap,
-    bg[i].bg_inode_bitmap,
-    bg[i].bg_inode_table,
-    bg[i].bg_free_blocks_count,
-    bg[i].bg_free_inodes_count,
-    bg[i].bg_used_dirs_count);
+           i,
+           bg[i].bg_block_bitmap,
+           bg[i].bg_inode_bitmap,
+           bg[i].bg_inode_table,
+           bg[i].bg_free_blocks_count,
+           bg[i].bg_free_inodes_count,
+           bg[i].bg_used_dirs_count);
   }
   printf("\n");
 }
@@ -47,12 +47,15 @@ void printBGDT(BlockGroup *bg, u32 totalBlockGroups) {
 void compareSuperBlocks(SuperBlock sb, SuperBlock sbCopy) {
   if (sb.s_inodes_count != sbCopy.s_inodes_count) {
     printf("Error: s_inodes_count\n");
+    sbCopy.s_inodes_count = sb.s_inodes_count;
   }
   if (sb.s_blocks_count != sbCopy.s_blocks_count) {
     printf("Error: s_blocks_count \n");
+    sbCopy.s_blocks_count = sb.s_blocks_count;
   }
   if (sb.s_r_blocks_count != sbCopy.s_r_blocks_count) {
     printf("Error: s_r_blocks_count \n");
+    sbCopy.s_r_blocks_count = sb.s_r_blocks_count;
   }
   if (sb.s_free_blocks_count != sbCopy.s_free_blocks_count) {
     printf("Error: s_free_blocks_count \n");
@@ -62,24 +65,31 @@ void compareSuperBlocks(SuperBlock sb, SuperBlock sbCopy) {
   }
   if (sb.s_first_data_block != sbCopy.s_first_data_block) {
     printf("Error: s_first_data_block \n");
+    sbCopy.s_first_data_block = sb.s_first_data_block;
   }
   if (sb.s_log_block_size != sbCopy.s_log_block_size) {
     printf("Error: s_log_block_size \n");
+    sbCopy.s_log_block_size = sb.s_log_block_size;
   }
   if (sb.s_log_frag_size != sbCopy.s_log_frag_size) {
     printf("Error: s_log_frag_size \n");
+    sbCopy.s_log_frag_size = sb.s_log_frag_size;
   }
-  if (sb.s_blocks_per_group!= sbCopy.s_blocks_per_group) {
+  if (sb.s_blocks_per_group != sbCopy.s_blocks_per_group) {
     printf("Error: s_blocks_per_group \n");
+    sbCopy.s_blocks_per_group = sb.s_blocks_per_group;
   }
   if (sb.s_frags_per_group != sbCopy.s_frags_per_group) {
     printf("Error: s_frags_per_group \n");
+    sbCopy.s_frags_per_group = sb.s_frags_per_group;
   }
   if (sb.s_inodes_per_group != sbCopy.s_inodes_per_group) {
     printf("Error: s_inodes_per_group \n");
+    sbCopy.s_inodes_per_group = sb.s_inodes_per_group;
   }
   if (sb.s_mtime != sbCopy.s_mtime) {
     printf("Error: s_mtime \n");
+    sbCopy.s_mtime = sb.s_mtime;
   }
   if (sb.s_wtime != sbCopy.s_wtime) {
     printf("Error: s_wtime \n");
@@ -89,75 +99,96 @@ void compareSuperBlocks(SuperBlock sb, SuperBlock sbCopy) {
   }
   if (sb.s_max_mnt_count != sbCopy.s_max_mnt_count) {
     printf("Error: s_max_mnt_count \n");
+    sbCopy.s_max_mnt_count = sb.s_max_mnt_count;
   }
   if (sb.s_magic != sbCopy.s_magic) {
     printf("Error: s_magic \n");
+    sbCopy.s_magic = sb.s_magic;
   }
   if (sb.s_state != sbCopy.s_state) {
     printf("Error: s_state \n");
   }
   if (sb.s_errors != sbCopy.s_errors) {
     printf("Error: s_errors \n");
+    sbCopy.s_errors = sb.s_errors;
   }
   if (sb.s_minor_rev_level != sbCopy.s_minor_rev_level) {
     printf("Error: s_minor_rev_level \n");
+    sbCopy.s_minor_rev_level = sb.s_minor_rev_level;
   }
   if (sb.s_lastcheck != sbCopy.s_lastcheck) {
     printf("Error: s_lastcheck \n");
+    sbCopy.s_lastcheck = sb.s_lastcheck;
   }
   if (sb.s_checkinterval != sbCopy.s_checkinterval) {
     printf("Error: s_checkinterval \n");
+    sbCopy.s_checkinterval = sb.s_checkinterval;
   }
   if (sb.s_creator_os != sbCopy.s_creator_os) {
     printf("Error: s_creator_os \n");
+    sbCopy.s_creator_os = sb.s_creator_os;
   }
   if (sb.s_rev_level != sbCopy.s_rev_level) {
     printf("Error: s_rev_level \n");
+    sbCopy.s_rev_level = sb.s_rev_level;
   }
   if (sb.s_def_resuid != sbCopy.s_def_resuid) {
     printf("Error: s_def_resuid \n");
+    sbCopy.s_def_resuid = sb.s_def_resuid;
   }
   if (sb.s_def_resgid != sbCopy.s_def_resgid) {
     printf("Error: s_def_resgid \n");
+    sbCopy.s_def_resgid = sb.s_def_resgid;
   }
   if (sb.s_first_ino != sbCopy.s_first_ino) {
     printf("Error: s_first_ino \n");
+    sbCopy.s_first_ino = sb.s_first_ino;
   }
   if (sb.s_inode_size != sbCopy.s_inode_size) {
     printf("Error: s_inode_size \n");
+    sbCopy.s_inode_size = sb.s_inode_size;
   }
   if (sb.s_block_group_nr != sbCopy.s_block_group_nr) {
     printf("Error: s_block_group_nr \n");
   }
   if (sb.s_feature_compat != sbCopy.s_feature_compat) {
     printf("Error: s_feature_compat \n");
+    sbCopy.s_feature_compat = sb.s_feature_compat;
   }
   if (sb.s_feature_incompat != sbCopy.s_feature_incompat) {
     printf("Error: s_feature_incompat \n");
+    sbCopy.s_feature_incompat = sb.s_feature_incompat;
   }
   if (sb.s_feature_ro_compat != sbCopy.s_feature_ro_compat) {
     printf("Error: s_feature_ro_compat \n");
+    sbCopy.s_feature_ro_compat = sb.s_feature_ro_compat;
   }
 }
 
-void compareBlockGroups(BlockGroup &bg, BlockGroup &bgCopy) {
+void compareBlockGroupTableCopies(BlockGroup &bg, BlockGroup &bgCopy) {
   if(bg.bg_block_bitmap != bgCopy.bg_block_bitmap) {
     printf("Error: bg_block_bitmap \n");
+    bgCopy.bg_block_bitmap = bg.bg_block_bitmap;
   }
   if(bg.bg_inode_bitmap != bgCopy.bg_inode_bitmap) {
     printf("Error: bg_inode_bitmap \n");
+    bgCopy.bg_inode_bitmap = bg.bg_inode_bitmap;
   }
   if(bg.bg_inode_table != bgCopy.bg_inode_table) {
     printf("Error: bg_inode_table \n");
+    bgCopy.bg_inode_table = bg.bg_inode_table;
   }
   if(bg.bg_free_blocks_count != bgCopy.bg_free_blocks_count) {
     printf("Error: bg_free_blocks_count \n");
+    bgCopy.bg_free_blocks_count = bg.bg_free_blocks_count;
   }
   if(bg.bg_free_inodes_count != bgCopy.bg_free_inodes_count) {
     printf("Error: bg_free_inodes_count \n");
+    bgCopy.bg_free_inodes_count = bg.bg_free_inodes_count;
   }
   if(bg.bg_used_dirs_count != bgCopy.bg_used_dirs_count) {
     printf("Error: bg_used_dirs_count \n");
+    bgCopy.bg_used_dirs_count = bg.bg_used_dirs_count;
   }
 }
 
@@ -217,7 +248,7 @@ void checkBlockGroupTableCopies(int fs, SuperBlock sb, BlockGroup *bg) {
       }
       for (int j = 0; j < totalBlockGroups; j++) {
         read(fs, &bgCopy[j], sizeof(BlockGroup));
-        compareBlockGroups(bgCopy[j], bgCopy[j]);
+        compareBlockGroupTableCopies(bgCopy[j], bgCopy[j]);
       }
     }
   }
@@ -296,10 +327,14 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < totalBlockGroups; i++) {
     read(fs, &bg[i], sizeof(BlockGroup));
   }
+
   // calculate number of inodes per block
   u32 inodesPerBlock = blockSize / sizeof(Inode);
+  printf("inodesPerBlock: %u\n", inodesPerBlock);
   // calculate size in blocks of the inode table
   u32 itableBlocks = sb.s_inodes_per_group / inodesPerBlock;
+  printf("itableBlocks: %u\n", itableBlocks);
+  u32 inodeSize = sizeof(Inode);
 
   printSuperBlock(sb);
   printBGDT(bg, totalBlockGroups);
